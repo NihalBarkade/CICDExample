e5f9cdbbb28f133e04a219d0ce832b201027b5f0
Nihal Barkade <nihalbarkade2003@gmail.com>
2025-07-16 17:07:15 +0530

    minor change to application.py

--- application.py
+++ application.py
@@ -32,5 +32,5 @@ def predictData():
         return render_template('home.html', results=results[0])
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
     
